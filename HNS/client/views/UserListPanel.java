95d603eff4454a938fb0980566c0e80446ba8a1e
MuhammadKhan <mhk42@njit.edu>
2023-05-05 14:26:40 -0400

    completed milestone4 & other changes

--- HNS/client/views/UserListPanel.java
+++ HNS/client/views/UserListPanel.java
@@ -1,11 +1,12 @@
 package HNS.client.views;
 
 import java.awt.BorderLayout;
-
+import java.awt.Color;
 import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.event.ContainerEvent;
 import java.awt.event.ContainerListener;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -17,11 +18,15 @@ import javax.swing.ScrollPaneConstants;
 
 import HNS.client.ClientUtils;
 import HNS.client.ICardControls;
+import HNS.server.ServerThread;
 
 public class UserListPanel extends JPanel {
     JPanel userListArea;
     JPanel wrapper;
     private static Logger logger = Logger.getLogger(UserListPanel.class.getName());
+
+
+    
 
     public UserListPanel(ICardControls controls) {
         super(new BorderLayout(10, 10));
@@ -42,6 +47,8 @@ public class UserListPanel extends JPanel {
         this.wrapper = wrapper;
         wrapper.add(scroll);
         this.add(wrapper, BorderLayout.CENTER);
+
+        
 
         userListArea.addContainerListener(new ContainerListener() {
 
@@ -77,12 +84,29 @@ public class UserListPanel extends JPanel {
         userListArea.repaint();
     }
 
+    ServerThread serverThread = new ServerThread();
+
+    List<String> myList = serverThread.getMyList();
+
+
+    
+
+
+
+
+
+
+
+
+
+
     protected void addUserListItem(long clientId, String clientName) {
+        
         logger.log(Level.INFO, "Adding user to list: " + clientName);
         JPanel content = userListArea;
         logger.log(Level.INFO, "Userlist: " + content.getSize());
         JEditorPane textContainer = new JEditorPane("text/plain", clientName);
-        textContainer.setName(clientId + "");
+        textContainer.setName(clientId + "" + clientName);
         // sizes the panel to attempt to take up the width of the container
         // and expand in height based on word wrapping
         textContainer.setLayout(null);
@@ -92,10 +116,32 @@ public class UserListPanel extends JPanel {
         textContainer.setEditable(false);
         // remove background and border (comment these out to see what it looks like
         // otherwise)
+        
+        setMuted(clientName);
         ClientUtils.clearBackground(textContainer);
         // add to container
         content.add(textContainer);
     }
+
+    public void setMuted(String clientName) {
+        Component[] cs = userListArea.getComponents();
+        for (Component c : cs) {
+                for (String str : myList) {
+                    if (str.equals(c)) {
+                        this.setBackground(Color.GRAY);
+                    }
+                }
+            
+        }
+    }
+
+    public void setLastUser(String clientName) {
+        Component[] cs = userListArea.getComponents();
+        for (Component c : cs) {
+
+    }
+    }
+
 
     protected void removeUserListItem(long clientId) {
         logger.log(Level.INFO, "removing user list item for id " + clientId);
